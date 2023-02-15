93a5f0be551a581db2d03df29b2a15c9b67e2622
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-02-15 15:34:09 -0300

    adding cors config for frontend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 import routes from "./router";
 
 const app = express();
 
+app.use(cors({
+    origin: "http://localhost:3001"
+}));
 app.use(express.json());
 app.use(routes);
 
