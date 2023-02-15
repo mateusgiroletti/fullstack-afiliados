54d5df9eb89dc889f815f03fdcb17b07c54f2c1e
Mateus Giroletti <mateusgiroletti97@gmail.com>
2023-02-15 10:43:39 -0300

    adding eslint config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <h1>Test</h1>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <h1>Test</h1>
+    </React.StrictMode>,
+);
